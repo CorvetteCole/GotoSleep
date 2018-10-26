1b681438a892135d65a2d2c06145ed115e156149
Cole Gerdemann <corvettecole@gmail.com>
2018-10-26 12:59:17 -0400

    starting some basic intro code

---
+++ app/src/main/java/com/corvettecole/gotosleep/IntroActivity.java
@@ -0,0 +1,60 @@
+package com.corvettecole.gotosleep;
+
+import android.graphics.Color;
+import android.os.Bundle;
+
+import com.github.paolorotolo.appintro.AppIntro2;
+import com.github.paolorotolo.appintro.AppIntroFragment;
+import com.github.paolorotolo.appintro.model.SliderPage;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class IntroActivity extends AppIntro2 {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Note here that we DO NOT use setContentView();
+
+        // Add your slide fragments here.
+        // AppIntro will automatically generate the dots indicator and buttons.
+        addSlide(firstFragment);
+        addSlide(secondFragment);
+        addSlide(thirdFragment);
+        addSlide(fourthFragment);
+
+        // Instead of fragments, you can also use our default slide.
+        // Just create a `SliderPage` and provide title, description, background and image.
+        // AppIntro will do the rest.
+        SliderPage sliderPage = new SliderPage();
+        sliderPage.setTitle(title);
+        sliderPage.setDescription(description);
+        sliderPage.setImageDrawable(image);
+        sliderPage.setBgColor(backgroundColor);
+        addSlide(AppIntroFragment.newInstance(sliderPage));
+
+        // Hide Skip/Done button.
+        showSkipButton(false);
+        setProgressButtonEnabled(false);
+
+    }
+
+    @Override
+    public void onSkipPressed(Fragment currentFragment) {
+        super.onSkipPressed(currentFragment);
+        // Do something when users tap on Skip button.
+    }
+
+    @Override
+    public void onDonePressed(Fragment currentFragment) {
+        super.onDonePressed(currentFragment);
+        // Do something when users tap on Done button.
+    }
+
+    @Override
+    public void onSlideChanged(@Nullable Fragment oldFragment, @Nullable Fragment newFragment) {
+        super.onSlideChanged(oldFragment, newFragment);
+        // Do something when the slide changes.
+    }
+}

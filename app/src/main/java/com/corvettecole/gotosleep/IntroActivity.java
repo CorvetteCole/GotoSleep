c56f28e890d2a5cf4b7642c52ffb5d14a692c652
CorvetteCole <corvettecole@gmail.com>
2018-10-26 19:43:29 -0400

    Added intro slides with placeholder icons

--- app/src/main/java/com/corvettecole/gotosleep/IntroActivity.java
+++ app/src/main/java/com/corvettecole/gotosleep/IntroActivity.java
@@ -1,42 +1,73 @@
 package com.corvettecole.gotosleep;
 
+import android.content.Intent;
 import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.FrameLayout;
 
+import com.github.paolorotolo.appintro.AppIntro;
 import com.github.paolorotolo.appintro.AppIntro2;
 import com.github.paolorotolo.appintro.AppIntroFragment;
 import com.github.paolorotolo.appintro.model.SliderPage;
+import com.google.android.material.snackbar.Snackbar;
 
+import androidx.annotation.ColorRes;
 import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
-public class IntroActivity extends AppIntro2 {
+public class IntroActivity extends AppIntro {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         // Note here that we DO NOT use setContentView();
-
-        // Add your slide fragments here.
-        // AppIntro will automatically generate the dots indicator and buttons.
-        addSlide(firstFragment);
-        addSlide(secondFragment);
-        addSlide(thirdFragment);
-        addSlide(fourthFragment);
-
         // Instead of fragments, you can also use our default slide.
         // Just create a `SliderPage` and provide title, description, background and image.
         // AppIntro will do the rest.
-        SliderPage sliderPage = new SliderPage();
-        sliderPage.setTitle(title);
-        sliderPage.setDescription(description);
-        sliderPage.setImageDrawable(image);
-        sliderPage.setBgColor(backgroundColor);
-        addSlide(AppIntroFragment.newInstance(sliderPage));
+        SliderPage firstSlide = new SliderPage();
+        firstSlide.setTitle("Welcome to Go to Sleep!");
+        firstSlide.setDescription("It's time to fix your sleep schedule");
+        firstSlide.setImageDrawable(R.drawable.ic_timelapse_white_24dp);
+        firstSlide.setBgColor(ContextCompat.getColor(this, R.color.firstSlide));
+
+        SliderPage secondSlide = new SliderPage();
+        secondSlide.setTitle("Remind yourself.");
+        secondSlide.setDescription("Set your bedtime and never lose track of time again");
+        secondSlide.setImageDrawable(R.drawable.ic_alarm_clock);
+        secondSlide.setBgColor(ContextCompat.getColor(this, R.color.secondSlide));
+
+
+        SliderPage thirdSlide = new SliderPage();
+        thirdSlide.setTitle("Motivate yourself.");
+        thirdSlide.setDescription("Set custom sleep reminders (and no vulgarity filter)");
+        thirdSlide.setImageDrawable(R.drawable.ic_list_white_24dp);
+        thirdSlide.setBgColor(ContextCompat.getColor(this, R.color.thirdSlide));
+
+
+        SliderPage fourthSlide = new SliderPage();
+        fourthSlide.setTitle("Let's do this.");
+        //fourthSlide.setDescription("");
+        fourthSlide.setImageDrawable(R.drawable.ic_airline_seat_individual_suite_white_24dp);
+        fourthSlide.setBgColor(ContextCompat.getColor(this, R.color.fourthSlide));
+
+
+        setColorTransitionsEnabled(true);
+        addSlide(AppIntroFragment.newInstance(firstSlide));
+        addSlide(AppIntroFragment.newInstance(secondSlide));
+        addSlide(AppIntroFragment.newInstance(thirdSlide));
+        addSlide(AppIntroFragment.newInstance(fourthSlide));
+
 
         // Hide Skip/Done button.
         showSkipButton(false);
-        setProgressButtonEnabled(false);
+        setProgressButtonEnabled(true);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
     }
 
@@ -49,12 +80,18 @@ public class IntroActivity extends AppIntro2 {
     @Override
     public void onDonePressed(Fragment currentFragment) {
         super.onDonePressed(currentFragment);
+        loadMainActivity();
         // Do something when users tap on Done button.
     }
 
     @Override
     public void onSlideChanged(@Nullable Fragment oldFragment, @Nullable Fragment newFragment) {
         super.onSlideChanged(oldFragment, newFragment);
-        // Do something when the slide changes.
+
+    }
+
+    private void loadMainActivity(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/corvettecole/gotosleep/MainActivity.java
+++ app/src/main/java/com/corvettecole/gotosleep/MainActivity.java
@@ -1,31 +1,63 @@
 package com.corvettecole.gotosleep;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        //  Declare a new thread to do a preference check
+        Thread t = new Thread(new Runnable() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void run() {
+                //  Initialize SharedPreferences
+                SharedPreferences getPrefs = PreferenceManager
+                        .getDefaultSharedPreferences(getBaseContext());
+
+                //  Create a new boolean and preference and set it to true
+                boolean isFirstStart = getPrefs.getBoolean("firstStart", true);
+
+                //  If the activity has never started before...
+                if (isFirstStart) {
+
+                    //  Launch app slide1
+                    final Intent i = new Intent(MainActivity.this, IntroActivity.class);
+
+                    runOnUiThread(new Runnable() {
+                        @Override public void run() {
+                            startActivity(i);
+                        }
+                    });
+
+                    //  Make a new preferences editor
+                    SharedPreferences.Editor e = getPrefs.edit();
+
+                    //  Edit preference to make it false because we don't want this to run again
+                    e.putBoolean("firstStart", false);
+
+                    //  Apply changes
+                    e.apply();
+                }
             }
         });
+
+        // Start the thread
+        t.start();
+        setContentView(R.layout.activity_main);
     }
 
     @Override

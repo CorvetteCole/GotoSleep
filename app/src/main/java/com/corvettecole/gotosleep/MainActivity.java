c9f32e8c3171137d26260afd60dffb3523660e4a
CorvetteCole <corvettecole@gmail.com>
2018-10-27 11:07:30 -0400

    added splash screen and app adaptive icon

--- app/src/main/java/com/corvettecole/gotosleep/MainActivity.java
+++ app/src/main/java/com/corvettecole/gotosleep/MainActivity.java
@@ -19,9 +19,11 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
+
         //  Declare a new thread to do a preference check
-        Thread t = new Thread(new Runnable() {
+        /*Thread t = new Thread(new Runnable() {
             @Override
             public void run() {
                 //  Initialize SharedPreferences
@@ -53,10 +55,38 @@ public class MainActivity extends AppCompatActivity {
                     e.apply();
                 }
             }
-        });
+        });*/
 
         // Start the thread
-        t.start();
+        //t.start();
+
+        SharedPreferences getPrefs = PreferenceManager
+                .getDefaultSharedPreferences(getBaseContext());
+
+        //  Create a new boolean and preference and set it to true
+        boolean isFirstStart = getPrefs.getBoolean("firstStart", true);
+
+        //  If the activity has never started before...
+        if (isFirstStart) {
+
+            //  Launch app slide1
+            final Intent i = new Intent(MainActivity.this, IntroActivity.class);
+
+            runOnUiThread(new Runnable() {
+                @Override public void run() {
+                    startActivity(i);
+                }
+            });
+
+            //  Make a new preferences editor
+            SharedPreferences.Editor e = getPrefs.edit();
+
+            //  Edit preference to make it false because we don't want this to run again
+            e.putBoolean("firstStart", false);
+
+            //  Apply changes
+            e.apply();
+        }
         setContentView(R.layout.activity_main);
     }
 

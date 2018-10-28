4f856921cdac4792d06b2ecfc454917d595fcb9d
CorvetteCole <corvettecole@gmail.com>
2018-10-27 22:53:42 -0400

    cleanup legacy code

--- app/src/main/java/com/corvettecole/gotosleep/IntroActivity.java
+++ app/src/main/java/com/corvettecole/gotosleep/IntroActivity.java
@@ -1,26 +1,13 @@
 package com.corvettecole.gotosleep;
 
 import android.content.Intent;
-import android.graphics.Color;
-import android.graphics.Typeface;
-import android.os.Build;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.DisplayCutout;
-import android.view.View;
-import android.view.WindowInsets;
 import android.view.WindowManager;
-import android.widget.Toast;
-
 import com.github.paolorotolo.appintro.AppIntro;
 import com.github.paolorotolo.appintro.AppIntroFragment;
 import com.github.paolorotolo.appintro.model.SliderPage;
-
-import java.util.Objects;
-
 import androidx.annotation.Nullable;
 import androidx.core.content.ContextCompat;
-import androidx.core.view.WindowInsetsCompat;
 import androidx.fragment.app.Fragment;
 
 public class IntroActivity extends AppIntro {
--- app/src/main/java/com/corvettecole/gotosleep/MainActivity.java
+++ app/src/main/java/com/corvettecole/gotosleep/MainActivity.java
@@ -2,25 +2,9 @@ package com.corvettecole.gotosleep;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.graphics.Color;
-import android.os.Build;
 import android.os.Bundle;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.core.view.WindowInsetsCompat;
-
 import android.preference.PreferenceManager;
-import android.util.Log;
-import android.view.DisplayCutout;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.WindowInsets;
-import android.view.WindowManager;
-
-import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -57,27 +41,5 @@ public class MainActivity extends AppCompatActivity {
             e.apply();
         }
         setContentView(R.layout.activity_main);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
